ddd0f4bf24d8257cbdd10179d1a7efa9b8411487
Triona Dwyer <CaitrionaNicLughadha@ncirl.ie>
2024-07-30 10:42:05 +0100

    first commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,67 @@
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+import Todo from './Todo'
+
+function TodoList() {
+	
+	// define todolist as an empty array and setTodoList as its update method 
+	const [todoList, setTodoList] = useState([]);
+	
+	// specifies behaviour to happen when the component is rendered
+	// we define a function to fetch the data from the server and
+	// put it into the todoList array
+	// then we call that function
+	
+	useEffect(() =>  {
+        async function fetchData() {		
+		try {
+			  const response = await axios.get("http://3.93.135.162:3000/todos", {
+				headers: { Accept: "application/json" }
+			  });
+			  console.log("RESPONSE DATA " + response.data);
+			  setTodoList(response.data);
+			} catch (error) {
+			  console.error('Error fetching todos:', error);
+			}
+		
+		
+		
+	}
+    fetchData(); 
+	},[] );  // empty brackets in 2nd argument of useEffect 
+	         // means it will run only on the initial render
+	
+	return ( 
+	<div>
+	    <ul class="demo">
+		{
+			
+			// map function iterates over an array
+			// it takes an argument of a function that will be called for each Element
+			// in the array. The function takes two arguments , i which is the current array element 
+			// and index which is the index of the current element
+			todoList.map(function(i,index) { 
+			console.log(i);
+			
+			return (
+			   <li key={index}>
+			   <Todo title={i.title} completed={i.completed} />
+			   </li> )
+			    
+			})
+		}
+		</ul>
+	    
+	</div>
+	
+	)
+	
+	/* const todoItems = todoList.map(todo =>
+	 <li key={todo.id}>{todo.title} </li>	
+	 );
+	 
+    return ( <ul> {todoItems}</ul> ) */
+}
+	
+
+export default TodoList;
